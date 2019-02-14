dcff255e8806f4c7169014557c011cb43da25a50
Zak <zaki.aroui@gmail.com>
2019-02-13 22:55:03 -0500

    added styles and js libraries

---
+++ src/main/java/com/ikazme/remote/BootstrapBasePage.java
@@ -0,0 +1,15 @@
+package com.ikazme.remote;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class BootstrapBasePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public BootstrapBasePage(final PageParameters parameters) {
+		super(parameters);
+
+
+
+	}
+}
